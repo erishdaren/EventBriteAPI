ef7c42fd4b7724f94f9c09ef4884f710ccee71c3
Erick Moreno <moreno240190@gmail.com>
2019-10-04 15:12:59 -0500

    Categorias en el state y formulario

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Class extends Component {
+
+    token = 'U4FDN53LXNFJ4C2PJ7XP&locale';
+    state = {
+      categorias : []
+    }
+    // inicia el sitio con los valores asignado en la funcion
+    componentDidMount() {
+      this.obtenerCategorias();
+    }
+    //funcion para obtener las cateogorias
+    obtenerCategorias = async () => {
+        let url = `https://www.eventbriteapi.com/v3/categories/?token=${this.token}=es_ES`;
+        await fetch(url)
+                .then(respuesta => {
+                  return respuesta.json();
+                })
+                .then(categorias => {
+                 this.setState({
+                   categorias : categorias.categories
+                 })
+                })
+    }
+    render() {
+        return (
+          <div className="App">
+                <Header titulo = "Eventos Sociales" />
+
+                <div className="uk-container">
+                    <Formulario 
+                      categorias = {this.state.categorias}
+                    />
+                </div>
+          </div>
+        );
+    }
 }
 
-export default App;
+
